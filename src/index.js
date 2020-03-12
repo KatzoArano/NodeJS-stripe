2dd2f37b53806395b44cdf7135214442096976c6
oarano <oarano@macbook-pro-de-oarano.home>
2020-03-12 06:35:40 +0100

    created structure routes/hbs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const expbhs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
-// Start server
+// Settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', expbhs({
+    defaultLayout : 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname : '.hbs'
+}));
 
+// Middelware
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
+
+// Routes
+app.use(require('./routes/index'))
+
+// Static
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Start server
 app.listen(3000, () => {
     console.log('Server listen', 3000);
 });
